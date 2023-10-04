01d079c0ddf2e57a3b06139463fc79dbc0eec4e0
joaovicenteft <joao.vicenteft@gmail.com>
2023-10-04 09:38:34 -0400

    creating the service layer adding the dependecy inversion of service abstraction layer autowired for future database connection

--- src/main/java/com/example/finance/controller/FinanceController.java
+++ src/main/java/com/example/finance/controller/FinanceController.java
@@ -3,11 +3,13 @@ package com.example.finance.controller;
 import com.example.finance.model.DataEntry;
 import com.example.finance.model.EntityFinance;
 import com.example.finance.model.financeRepository;
+import com.example.finance.service.FinanceService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.parser.Entity;
 import java.sql.Array;
 import java.util.*;
 
@@ -16,7 +18,7 @@ import java.util.*;
 public class FinanceController {
 
     @Autowired
-    private financeRepository financerepo;
+    private FinanceService financeService;
 
     private final Map<Long, List<String>> financeMap = new HashMap<>();
     private final List<DataEntry> financeList = new ArrayList<>();
@@ -25,72 +27,40 @@ public class FinanceController {
     @GetMapping
     public ResponseEntity<List<EntityFinance>> getAllFinance() {
         List<EntityFinance> entityFinance = new ArrayList<>();
-        System.out.println(financeList);
-        for (DataEntry entry : financeList) {
-            entityFinance.add(new EntityFinance(Long.parseLong(entry.getValues(0)), Integer.parseInt(entry.getValues(1)),
-                    Double.parseDouble(entry.getValues(2)), Double.parseDouble(entry.getValues(3))));
-        }
+
+        entityFinance = financeService.findAll(financeList, entityFinance);
 
         return ResponseEntity.ok(entityFinance);
     }
     
     @GetMapping("/{id}")
-    public ResponseEntity<EntityFinance> getFinanceById(@PathVariable Long id) {
-        //List<String> message = financeMap.get(id);
-        //System.out.println(financeList.get(id.intValue()-1).getValues(0));
+    public ResponseEntity<List<EntityFinance>> getFinanceById(@PathVariable Long id) {
+        List<EntityFinance> entityFinance = new ArrayList<>();
+        entityFinance = financeService.findSpecificId(financeList, id, entityFinance);
 
-        String id_financeList = null;
-        String timeMonth = null;
-        String inflationMean = null;
-        String fullMoney = null;
-
-        for (int i = 0; i < financeList.size(); i++) {
-            //System.out.println(financeList.get(i).getValues(0));
-            
-            if (Long.parseLong(financeList.get(i).getValues(0)) == id) {
-                id_financeList = financeList.get(i).getValues(0); // equivalent to take position id with index
-                timeMonth = financeList.get(i).getValues(1); // values
-                inflationMean = financeList.get(i).getValues(2); // more values
-                fullMoney = financeList.get(i).getValues(3);
-            }
-        }
-        
-
-        if (timeMonth != null) {
-            return ResponseEntity.ok(new EntityFinance(Long.parseLong(id_financeList), Integer.parseInt(timeMonth)
-                    , Double.parseDouble(inflationMean), Double.parseDouble(fullMoney)));
-        }
-        String strid = String.valueOf(id);
-        List<String> stringList = new ArrayList<>();
-
-        for (int i = 0; i < strid.length(); i++) {
-            stringList.add(Character.toString(strid.charAt(i)));
+        if (entityFinance != null) {
+            return ResponseEntity.ok(entityFinance);
         }
 
-        Iterable<String> stringIterable = stringList;
-
-        financerepo.findAllById(stringIterable);
         return ResponseEntity.notFound().build();
     }
 
 
     @PostMapping
     public ResponseEntity<EntityFinance> createFinance(@RequestBody EntityFinance entityfinance) {
-        //List<String> values = Arrays.asList(entityfinance.getValues(), entityfinance.getTimeMonth());
-        //financeMap.put(idCounter, values);
-        financeList.add(new DataEntry(String.valueOf(idCounter),
-                String.valueOf(entityfinance.getTimeMonth()), String.valueOf(entityfinance.getInflationMean()), String.valueOf(entityfinance.getFullMoney())));
-        entityfinance.setId(idCounter);
+        EntityFinance entityFinance = financeService.postData(financeList, entityfinance, idCounter);
         idCounter++;
-        financerepo.save(entityfinance);
         return ResponseEntity.ok(entityfinance);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<EntityFinance> updateFinance(@PathVariable Long id, @RequestBody EntityFinance entityfinance) {
-        if (financeList.get(id.intValue()) != null) {
-            financeList.get(id.intValue()).putValues(id.intValue(), String.valueOf(entityfinance.getTimeMonth()));
-            return ResponseEntity.ok(new EntityFinance(id, entityfinance.getTimeMonth(), entityfinance.getInflationMean(), entityfinance.getFullMoney()));
+
+        EntityFinance entityFinance;
+        entityFinance = financeService.putData(financeList, id, entityfinance);
+
+        if (entityFinance != null) {
+            return ResponseEntity.ok(entityFinance);
         }
         return ResponseEntity.notFound().build();
     }
@@ -98,11 +68,9 @@ public class FinanceController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteFinance(@PathVariable Long id) {
 
-        if (financeList.get((id.intValue())-1) != null)  {
-            //financeList.get(id.intValue()-1).deleteValues(id.intValue()-1);
-            financeList.remove(id.intValue()-1);
+        if (financeService.deleteData(financeList, id) == 1)
             return ResponseEntity.noContent().build();
-        }
+
         return ResponseEntity.notFound().build();
     }
 }
---
+++ src/main/java/com/example/finance/service/FinanceService.java
@@ -0,0 +1,94 @@
+package com.example.finance.service;
+
+import com.example.finance.model.DataEntry;
+import com.example.finance.model.EntityFinance;
+import com.example.finance.model.financeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@EnableCaching
+public class FinanceService {
+
+    @Autowired
+    financeRepository financeRepo;
+
+    public List<EntityFinance> findAll(List<DataEntry> financeList, List<EntityFinance> entityFinance) {
+        for (DataEntry entry : financeList) {
+            entityFinance.add(new EntityFinance(Long.parseLong(entry.getValues(0)), Integer.parseInt(entry.getValues(1)),
+                    Double.parseDouble(entry.getValues(2)), Double.parseDouble(entry.getValues(3))));
+        }
+        return entityFinance;
+    }
+
+    public List<EntityFinance> findSpecificId(List<DataEntry> financeList, Long id, List<EntityFinance> entityFinance) {
+        //List<String> message = financeMap.get(id);
+        //System.out.println(financeList.get(id.intValue()-1).getValues(0));
+        String id_financeList = null;
+        String timeMonth = null;
+        String inflationMean = null;
+        String fullMoney = null;
+
+        for (int i = 0; i < financeList.size(); i++) {
+            //System.out.println(financeList.get(i).getValues(0));
+
+            if (Long.parseLong(financeList.get(i).getValues(0)) == id) {
+                id_financeList = financeList.get(i).getValues(0); // equivalent to take position id with index
+                timeMonth = financeList.get(i).getValues(1); // values
+                inflationMean = financeList.get(i).getValues(2); // more values
+                fullMoney = financeList.get(i).getValues(3);
+            }
+        }
+
+        entityFinance.add(new EntityFinance(Long.parseLong(id_financeList), Integer.parseInt(timeMonth)
+                , Double.parseDouble(inflationMean), Double.parseDouble(fullMoney)));
+
+        if (timeMonth != null) {
+            return entityFinance;
+        }
+        return null;
+        //String strid = String.valueOf(id);
+        //List<String> stringList = new ArrayList<>();
+
+        //for (int i = 0; i < strid.length(); i++) {
+        //  stringList.add(Character.toString(strid.charAt(i)));
+        //}
+
+        //Iterable<String> stringIterable = stringList;
+        //financerepo.findAllById(stringIterable);
+    }
+
+    public EntityFinance putData(List<DataEntry> financeList, Long id, EntityFinance entityfinance) {
+
+        if (financeList.get(id.intValue()) != null) {
+            financeList.get(id.intValue()).putValues(id.intValue(), String.valueOf(entityfinance.getTimeMonth()));
+            return (new EntityFinance(id, entityfinance.getTimeMonth(), entityfinance.getInflationMean(), entityfinance.getFullMoney()));
+        }
+        return null;
+    }
+
+    public EntityFinance postData(List<DataEntry> financeList, EntityFinance entityfinance, Long idCounter) {
+        //List<String> values = Arrays.asList(entityfinance.getValues(), entityfinance.getTimeMonth());
+        //financeMap.put(idCounter, values);
+        financeList.add(new DataEntry(String.valueOf(idCounter),
+                String.valueOf(entityfinance.getTimeMonth()), String.valueOf(entityfinance.getInflationMean()), String.valueOf(entityfinance.getFullMoney())));
+        entityfinance.setId(idCounter);
+
+        return entityfinance;
+        //financerepo.save(entityfinance);
+    }
+
+    public int deleteData(List<DataEntry> financeList, Long id) {
+        if (financeList.get((id.intValue())-1) != null)  {
+            //financeList.get(id.intValue()-1).deleteValues(id.intValue()-1);
+            financeList.remove(id.intValue()-1);
+            return 1;
+        }
+        return 0;
+    }
+}
